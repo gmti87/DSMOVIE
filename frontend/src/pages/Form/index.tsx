538ff1ee18c1147942c3094b59bb5165f2506728
Gabriel Moreti Oliveira <69165202+gmti87@users.noreply.github.com>
2022-01-14 20:54:30 -0300

    Update index.tsx

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -3,17 +3,6 @@ import { useParams } from 'react-router-dom';
 
 function Form() {
 
-    {/*
-    filme mocado 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-    */}
-
     const params = useParams();
 
     return (
