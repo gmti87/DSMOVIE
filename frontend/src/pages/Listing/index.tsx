d563ea147cfea2a38338dc01cd1553798aa11012
gmti87 <gmoreti87@gmail.com>
2022-01-14 18:22:55 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -32,7 +32,7 @@ function Listing() {
     */}
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
         .then(response => {
             const data = response.data as MoviePage;
             setPage(data);
@@ -63,8 +63,6 @@ function Listing() {
                         </div>
                     )
                     )}
-
-                    
                 </div>
             </div>
         </>
